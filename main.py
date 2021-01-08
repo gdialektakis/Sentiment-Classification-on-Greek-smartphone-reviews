801dd97820c493d28f52275be40387f4600866fa
George Dialektakis <geo4diale@gmail.com>
2021-01-08 19:35:39 +0200

    Implemented Multinomial Regression. Not Tested yet!!

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import text_preprocessing as tp
+from multinomialRegression import logistic_regression
+
 
 if __name__ == "__main__":
     print("Sentiment analysis on Greek Smartphone Reviews")
     pd = tp.preprocess()
     print(pd)
 
-    bow = tp.bag_of_words(pd)
-    print(bow)
+    # split dataset
+    x_train, x_test, y_train, y_test = tp.sklearn_train_test(pd)
+    # Leave-One-Out Cross-Validation
+    # cv_x_train, cv_x_test, cv_y_train, cv_y_test = tp.crossValidation(pd_df)
+    X_train_bow, X_test_bow = tp.bag_of_words(x_train, x_test)
+    X_train_tf_idf, X_test_tf_idf = tp.tf_idf(x_train, x_test)
+
+    logistic_regression(X_train_bow, X_test_bow, y_train, y_test, 'bag_of_words')
+    logistic_regression(X_train_bow, X_test_bow, y_train, y_test, 'tf-idf')
--- multinomialRegression.py
+++ multinomialRegression.py
@@ -1,8 +1,15 @@
-import text_preprocessing as tp
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.linear_model import LogisticRegression
-import numpy as np
 
-data = tp.preprocess()
-model = LogisticRegression(multi_class='multinomial')
+
+def logistic_regression(X_train, X_test, y_train, y_test, vectorization):
+    model = LogisticRegression(multi_class='multinomial').fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    print("Multinomial Logistic Regression using %s" % vectorization)
+    print('Accuracy of Logistic Regression: %s' % accuracy_score(y_pred, y_test))
+    print('Precision of Logistic Regression: %s' % precision_score(y_pred, y_test))
+    print('Recall of Logistic Regression: %s' % recall_score(y_pred, y_test))
+    print('F1_score of Logistic Regression: %s' % f1_score(y_pred, y_test))
+
+    return
