8607fcd91eb156fce68c84efcfbbd38481c8d9be
xrhstosg <georgitsis1996@gmail.com>
2021-01-10 15:04:38 +0200

    Add SVM

---
+++ SVM.py
@@ -0,0 +1,47 @@
+from sklearn.svm import SVC
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class Multi_SVM:
+    def __init__(self, kernel, gamma, C, degree, decision_function_shape):
+        self.data = []
+        self.kernel = kernel
+        self.gamma = gamma
+        self.C = C
+        self.degree = degree
+        self.decision_function_shape = decision_function_shape
+        if kernel == "poly":
+            self.model = SVC(kernel=kernel, gamma=gamma, C=C, degree=degree, decision_function_shape=decision_function_shape)
+        else:
+            self.model = SVC(kernel=kernel, gamma=gamma, C=C, decision_function_shape=decision_function_shape)
+
+    def run(self, x_train, x_test, y_train, y_test):
+
+
+        self.model.fit(x_train, y_train)
+        y_predicted = self.model.predict(x_test)
+
+
+        accuracy = metrics.accuracy_score(y_test, y_predicted)
+        recall = metrics.recall_score(y_test, y_predicted, average="macro")
+        precision = metrics.precision_score(y_test, y_predicted, average="macro")
+        f1 = metrics.f1_score(y_test, y_predicted, average="macro")
+
+        print("==========================================================")
+        print(self.kernel)
+        print("SVM Accuracy: %2f" % accuracy)
+        print("SVM Precision: %2f" % precision)
+        print("SVM Recall: %2f" % recall)
+        print("SVM F1 Score: %2f" % f1)
+
+        poly_cm = confusion_matrix(y_test, y_predicted, labels=[1, 2, 3, 4, 5])
+        print(poly_cm)
+        sns.heatmap(poly_cm)
+        plt.xlabel("true label")
+        plt.ylabel("predicted label")
+        plt.show()
+        print("==========================================================")
+
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import text_preprocessing as tp
 import naive_bayes
 from multinomialRegression import logistic_regression
-
+import SVM
 
 if __name__ == "__main__":
     print("Sentiment analysis on Greek Smartphone Reviews")
     df = tp.preprocess()
     print(df)
     x_train, x_test, y_train, y_test = tp.sklearn_train_test(df, sampling="oversample")
-    # Leave-One-Out Cross-Validation
-    # cv_x_train, cv_x_test, cv_y_train, cv_y_test = tp.crossValidation(pd_df)
+
     print("Bag of Words")
     bow_x_train, bow_x_test = tp.bag_of_words(x_train, x_test)
     bow_nb = naive_bayes.NaiveBayes(0.1)
@@ -18,9 +17,18 @@ if __name__ == "__main__":
     tfidf_nb = naive_bayes.NaiveBayes(0.1)
     tfidf_x_train, tfidf_x_test = tp.tf_idf(x_train, x_test)
     naive_bayes_results = tfidf_nb.run(tfidf_x_train, tfidf_x_test, y_train, y_test)
-    # Leave-One-Out Cross-Validation
-    # cv_x_train, cv_x_test, cv_y_train, cv_y_test = tp.crossValidation(pd_df)
 
+    print("TF-IDF FOR Polynomial SVM")
+    tfidf_poly_svm = SVM.Multi_SVM("poly", 6, 1, 5, "ovo")
+    tfidf_x_train, tfidf_x_test = tp.tf_idf(x_train, x_test)
+    poly_svm_results = tfidf_poly_svm.run(tfidf_x_train, tfidf_x_test, y_train, y_test)
+    print("FINISH TF-IDF FOR Polynomial SVM")
+
+    print("TF-IDF FOR Sigmoid SVM")
+    tfidf_sigmoid_svm = SVM.Multi_SVM("sigmoid", 2, 10, "ovo")
+    tfidf_x_train, tfidf_x_test = tp.tf_idf(x_train, x_test)
+    sigmoid_svm_results = tfidf_sigmoid_svm.run(tfidf_x_train, tfidf_x_test, y_train, y_test)
+    print("FINISH TF-IDF FOR Sigmoid SVM")
 
     print("New prediction on the trained model")
     print("====================================")
@@ -38,4 +46,3 @@ if __name__ == "__main__":
 
     logistic_regression(bow_x_train, bow_x_test, y_train, y_test, 'bag_of_words')
     logistic_regression(tfidf_x_train, tfidf_x_test, y_train, y_test, 'tf-idf')
-
