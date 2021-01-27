a367a7a1743abfdda6952c6e4ba5963a70dfbb85
George Dialektakis <geo4diale@gmail.com>
2021-01-27 12:01:05 +0200

    Optimized LSTM and plot training-validation curves.

--- lstm.py
+++ lstm.py
@@ -1,10 +1,12 @@
-from sklearn.utils import class_weight
+import keras
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
 from tensorflow.python.keras.callbacks import EarlyStopping
 from keras import Sequential
-from keras.layers import Dense, Embedding, LSTM, Bidirectional, Dropout
-from sklearn import model_selection, metrics
-from sklearn.metrics import accuracy_score
-import numpy as np
+from keras.layers import Dense, Embedding, LSTM, Bidirectional, Dropout, Activation
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.utils import compute_class_weight
 
 
 class LSTMNetwork:
@@ -21,45 +23,51 @@ class LSTMNetwork:
         embed_dim = 64
         lstm_out = 32
         model = Sequential()
+
         if not self.use_embeddings:
-            model.add(Embedding(self.max_features, embed_dim, input_length=X.shape[1]))
+            model.add(Embedding(self.max_features, embed_dim, input_length=X.shape[1], dropout=0.2))
         else:
             model.add(
-                Embedding(vocab_size, 300, weights=[self.embedding_matrix], input_length=X.shape[1], trainable=False))
+                Embedding(vocab_size, 300, weights=[self.embedding_matrix], input_length=X.shape[1], trainable=True,
+                          mask_zero=True, dropout=0.2))
         if self.is_bidirectional:
-            model.add(Bidirectional(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2)))
+            model.add(Bidirectional(LSTM(lstm_out)))
         else:
-            model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
+            model.add(LSTM(lstm_out))
+
+        model.add(Dropout(0.4))
+        model.add(Activation('relu'))
         model.add(Dense(5, activation='softmax'))
 
-        model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        optimizer = keras.optimizers.Adam(learning_rate=0.01)
+
+        model.compile(loss='sparse_categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
         print(model.summary())
+
         Y = df['rating'].values
         Y = np.array([int(v) - 1 for v in Y])
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, stratify=Y, test_size=0.1)
 
-        X_train_data, X_test_data, Y_train_data, Y_test_data = model_selection.train_test_split(X, Y, stratify=Y,
-                                                                                                test_size=0.1)
         if self.use_class_weights:
-            class_weights = class_weight.compute_class_weight('balanced',
-                                                              np.unique(Y_train_data), Y_train_data)
+            class_weights = compute_class_weight('balanced', np.unique(Y_train), Y_train)
         else:
             class_weights = None
 
-        batch_size = 32
-        model.fit(X_train_data,
-                  Y_train_data,
-                  epochs=4,
-                  batch_size=batch_size,
-                  validation_split=0.1,
-                  shuffle=True,
-                  class_weight=class_weights,
-                  callbacks=[EarlyStopping(monitor='val_accuracy',
-                                           min_delta=0.001,
-                                           patience=2,
-                                           verbose=1)]
-                  )
-        predictions_nn_train = model.predict(X_train_data)
-        predictions_nn_test = model.predict(X_test_data)
+        history = model.fit(X_train,
+                            Y_train,
+                            epochs=5,
+                            batch_size=64,
+                            validation_split=0.1,
+                            shuffle=True,
+                            class_weight=class_weights,
+                            callbacks=[EarlyStopping(monitor='loss',
+                                                     min_delta=0.0005,
+                                                     patience=2,
+                                                     verbose=1)]
+                            )
+        predictions_nn_train = model.predict(X_train)
+        predictions_nn_test = model.predict(X_test)
 
         y_predicted_test = []
         for prediction in predictions_nn_test:
@@ -67,16 +75,32 @@ class LSTMNetwork:
             y_predicted_test.append(result)
 
         y_predicted_train = []
-
         for prediction in predictions_nn_train:
             result = np.argmax(prediction)
             y_predicted_train.append(result)
 
-        print('Train accuracy:', accuracy_score(Y_train_data, y_predicted_train))
-        print('Test accuracy', accuracy_score(Y_test_data, y_predicted_test))
-        precision = metrics.precision_score(Y_test_data, y_predicted_test, average="micro")
-        recall = metrics.recall_score(Y_test_data, y_predicted_test, average="micro")
-        f1 = metrics.f1_score(Y_test_data, y_predicted_test, average="micro")
-        print('Test Precision: %2f' % precision)
-        print('Test Recall: %2f' % recall)
-        print('Test F1: %2f' % f1)
+        # Plot history: Cross Entropy Loss
+        plt.plot(history.history['loss'], label='training data')
+        plt.plot(history.history['val_loss'], label='validation data')
+        plt.xticks(np.arange(1, 6, step=1))
+        plt.title('Cross Entropy Loss')
+        plt.ylabel('Loss value')
+        plt.xlabel('No. epoch')
+        plt.legend(loc="upper right")
+        plt.show()
+
+        # plot accuracy
+        plt.plot(history.history['accuracy'], label='training data')
+        plt.plot(history.history['val_accuracy'], label='validation data')
+        plt.xticks(np.arange(1, 6, step=1))
+        plt.title('Accuracy')
+        plt.ylabel('Accuracy')
+        plt.xlabel('No. epoch')
+        plt.legend(loc="lower right")
+        plt.show()
+
+        print('Train accuracy: %.4f' % accuracy_score(Y_train, y_predicted_train))
+        print('Test accuracy: %.4f' % accuracy_score(Y_test, y_predicted_test))
+        print('Test Precision: %.4f' % precision_score(Y_test, y_predicted_test, average="micro"))
+        print('Test Recall: %.4f' % recall_score(Y_test, y_predicted_test, average="micro"))
+        print('Test F1: %.4f' % f1_score(Y_test, y_predicted_test, average="micro"))
